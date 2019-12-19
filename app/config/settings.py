c96f2e728a0b0cb654f42ea7338561c5c79bd2db
jihye <zehye.01@gmail.com>
2019-12-19 14:28:32 +0900

    many to many 앱생성 후 migrations 후 인스턴스 생성

--- app/config/settings.py
+++ app/config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'myapp.apps.MyappConfig',
     'many_to_one.apps.ManyToOneConfig',
+    'many_to_many.apps.ManyToManyConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ app/many_to_many/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=30)
+    toppings = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        return self.name
+
