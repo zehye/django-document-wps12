78159f77c96d00ca33c14f03351bd0e315d2a4ef
jihye <zehye.01@gmail.com>
2019-12-23 14:16:20 +0900

    related name 역참조 안하는 방법 정리

--- app/many_to_one/models/default.py
+++ app/many_to_one/models/default.py
@@ -10,6 +10,7 @@ class Manufacturer(models.Model):
 
 class Car(models.Model):
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    # 역방향 참조는 기본적으로 지원해주지만 필요없는 경우 related_name = '+'를 해주면 역방향참조가 되지 않는다.
     name = models.CharField('차 이름', max_length=100)
 
     def __str__(self):
